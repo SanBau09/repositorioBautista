5b9a59d32761dbdd6fcf9d6ecee65ededd077ea6
Sandra <sandrabautistacencerrado@gmail.com>
2024-04-03 17:10:44 +0200

    fin de la seccion2: primeros pasos en Angular

--- src/app/clientes/clientes.component.spec.ts
+++ src/app/clientes/clientes.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { ClientesComponent } from './clientes.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('ClientesComponent', () => {
+  let component: ClientesComponent;
+  let fixture: ComponentFixture<ClientesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ ClientesComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(ClientesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+
+    listaCurso: string[] = ['TypeScript', 'JavaScript', 'CSS', 'PHP'];
+
+    habilitar: boolean = true;
+
+    constructor() {};
+
+    setHabilitar(): void {
+      this.habilitar = (this.habilitar == true)? false: true;
+    }
+}
